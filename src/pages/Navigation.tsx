f08c4255fc5f039a71ea1a752e4b243ec8845161
Leonardo <leofranzinribeiro@gmail.com>
2024-10-28 21:44:03 -0300

    Navegacao e pagina de login feitas (Em TS)

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '../App.jsx';
+import Login from './Login.js';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />
+    }, {
+        path: "/login",
+        element: <Login />
     }
 ]);
 

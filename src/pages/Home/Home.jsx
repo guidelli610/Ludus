69ee179d8192b1ac1f944806ddeacbeae6bf49ac
guidelli610 <mateusguidellicabral@gmail.com>
2024-10-29 22:04:54 -0300

    home V0.01

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,15 @@
 import './Home.css';
-
-export default function Home() {
-    return <h1 className='test'> test </h1>
-};
+export default function HomeLateralLogo() {
+    return (
+        <div className="home_container">
+            <div className="home_image"> {/*separador para a img*/}
+                <img src="./assetsHome/hotbar_back.jpg" alt="Logo ludus" />
+            </div>
+            <div className="home_content">{/*conteudo lateral*/}
+                <h1>LUDUS</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestias impedit odio laudantium adipisci recusandae vitae quod nostrum veritatis, earum temporibus laborum aut autem possimus animi culpa deleniti magni libero.</p>
+            </div>
+        </div>
+    );
+}
+       
